7adb7964567dc4d2fe0f595165abd30ac2188184
mehdi-mojadam <mehdimojada@gmail.com>
2023-10-07 05:51:00 -0700

    add flex box

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FlexBox from './Components/FlexBox';
+
+function App() {
+  return (
+
+
+
+
+<FlexBox/>
+  );
+}
+
+export default App;
---
+++ src/Components/FlexBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../StyleComponent/style.css'
+function FlexBox() {
+  return (
+    <div className="container">
+        <div className="box" id="box1">1</div>
+        <div className="box" id="box2">2</div>
+        <div className="box" id="box3">3</div>
+        <div className="box" id="box4">4</div>
+        
+    </div>
+  )
+}
+
+export default FlexBox
